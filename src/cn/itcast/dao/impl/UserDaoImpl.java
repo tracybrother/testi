cc4edc38ec25079ba0facea62ef688b059009361
ly <tracy_brother@163.com>
2017-09-11 14:45:09 +0800

    1234

--- src/cn/itcast/dao/impl/UserDaoImpl.java
+++ src/cn/itcast/dao/impl/UserDaoImpl.java
@@ -9,7 +9,7 @@ import cn.itcast.dao.UserDao;
 import cn.itcast.domain.User;
 
 public class UserDaoImpl implements UserDao {
-	//sqlsessionFactory
+	//sqlsessionFact   ory
 	private SqlSessionFactory sqlSessionFactory;
 	public UserDaoImpl(SqlSessionFactory sqlSessionFactory){
 		
